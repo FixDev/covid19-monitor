0b0ce23450fcce26537f4801d99011606c3334af
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2020-03-22 10:58:38 +0700

    mentahan

---
+++ components/chart.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-card-text>{{ value }}</b-card-text>
+</template>
+
+<script>
+export default {
+  nama: {
+    type: String,
+    default: ""
+  },
+  value: {
+    type: String,
+    default: ""
+  }
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-card-group deck>
+      <b-card
+        v-for="item in items"
+        :key="item.value"
+        border-variant="primary"
+        header="Confirmed"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        align="center"
+      >
+        <b-card-text>{{ item.value }}</b-card-text>
+        <b-card-text>{{ item.detail }}</b-card-text>
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      const res = await this.$axios.get("https://covid19.mathdro.id/api/");
+      this.items = res.data;
+    }
+  }
+};
+</script>
+
+<style></style>
